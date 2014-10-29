869931fd29dd5ec5b26b10c2716ada95cde5efaa
unknown <warrock.boy@hotmail.com>
2014-10-29 17:16:12 +0100

    Theo added, gameBuyNow screenshot added

---
+++ unity-game/Assets/scripts/ParticleCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleCollision : MonoBehaviour {
+
+    void OnParticleCollision(GameObject other) {
+        Rigidbody body = other.rigidbody;
+        if (body) {
+            Vector3 direction = other.transform.position - transform.position;
+            direction = direction.normalized;
+            body.AddForce(direction * 5);
+        }
+    }
+
+}
